40ba60d4b1303a71991210a15e8f6e81a61233a8
sabrinapedroso <sabrina.apedroso1@gmail.com>
2022-11-28 16:52:40 -0300

    1.0

---
+++ js/ex02.js
@@ -0,0 +1,17 @@
+var frase, n1, n2, soma
+
+frase="Olá Mundo, essa é uma string"
+alert(frase)
+
+n1=3
+n2=5.5
+soma = n1 + n2
+alert(soma)
+
+document.write("<h2>"+soma+ "</h2>")
+
+n1=prompt("Insira um valor:")
+n2=prompt("Insira outro valor:")
+subtração = parseInt(n1) + parseInt(n2)
+alert("Resultado: "+soma)
+
---
+++ js/ex10.js
@@ -0,0 +1,17 @@
+var v, aux, lista
+
+v=[4, 8, 16, 32]
+alert(v)
+
+v[1]=20
+alert(v)
+alert(v[3])
+
+lista=["sabrina", 16, "ifpr", "TI3V", 4.7]
+alert(lista)
+
+lista.push("musica") //adiciona um item por último na lista
+alert(lista)
+
+lista.pop() //remove o último item da lista
+alert(lista)
